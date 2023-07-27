beb7f70e03ec29c1203dc8cf60a080137d3cf8a4
matshidisolp <mpitswane@gmail.com>
2023-07-27 05:54:14 -0700

    Checker 6 debugged

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
+		{
 			printf("[0] (nil)\n");
+			s += s;
+		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
