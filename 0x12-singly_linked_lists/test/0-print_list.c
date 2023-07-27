0dafea3cce391f99c8993a5395d3e85dcef3b975
matshidisolp <mpitswane@gmail.com>
2023-07-27 10:25:20 -0700

    Optimized task 2

---
+++ 0x12-singly_linked_lists/test/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of the list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of printed nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+			a += a;
+		}
+		else
+			if (h->str[0] == '%')
+				printf("[-1] %s\n", h->str);
+			else
+				printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		a++;
+	}
+
+	return (a);
+}
