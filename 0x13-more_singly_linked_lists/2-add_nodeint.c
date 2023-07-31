d3d6462fa58470d6d776b404d6e07fda4abb2898
matshidisolp <mpitswane@gmail.com>
2023-07-31 20:47:15 +0200

    0x13 More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
+ * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to the first node in the list
  * @n: data to insert in that new node
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new = malloc(sizeof(listint_t));
 
-	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
