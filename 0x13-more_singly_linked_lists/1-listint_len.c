0fd116f95589e58ee2731459df50b93eeee6786c
matshidisolp <mpitswane@gmail.com>
2023-07-31 20:24:21 +0200

    0x13 More singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,19 +2,19 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * @h: linked list of type listint_t
  *
- * Return: number of nodes
+ * Return: number of nodes/elements
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		num++;
+		count++;
 		h = h->next;
 	}
 
-	return (num);
+	return (count);
 }
