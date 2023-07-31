c6bf95e68b213f4d1f7a0cefbc9a68eb4169454e
matshidisolp <mpitswane@gmail.com>
2023-07-31 21:37:37 +0200

    0x13 More singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *current;
 
 	while (head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		current = head;
+		head = head->next;
+		free (current);
 	}
 }
