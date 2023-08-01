60dc04bb3831c9067ed142d5a58a61314ec95c37
matshidisolp <mpitswane@gmail.com>
2023-08-01 11:05:07 +0200

    0x13 More singly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * sum_listint - calculates the sum of all the data in a listint_t list
- * @head: first node in the linked list
+ *sum_listint - calculates the sum of all the data in list
+ *@head: first node in the linked list
  *
- * Return: resulting sum
+ *Return: the sum (total of n data of nodes)
  */
 int sum_listint(listint_t *head)
 {
