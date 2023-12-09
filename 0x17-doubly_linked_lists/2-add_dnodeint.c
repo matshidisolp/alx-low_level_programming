8510dc84b35f836709109d5cc18859ba09fbc9f6
matshidisolp <mpitswane@gmail.com>
2023-12-09 09:59:52 +0700

    task 0 add_nodeint

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
   * add_dnodeint - adds a new node at start of list
   * @head: head of the list
-  * @n: elemtn value
+  * @n: element value
   * Return: the address of the new element
   */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
