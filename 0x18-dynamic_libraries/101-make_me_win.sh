f41346510a78f95fd21020a96b58e052d1aca675
matshidisolp <mpitswane@gmail.com>
2023-12-13 09:28:56 +0700

    hack injection script updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githuhusercontent/matshidisolp/alx-low_level_programming/master/0x18-dynamic_libraries/100-operations.so
+wget -P .. https://raw.githubusercontent/matshidisolp/alx-low_level_programming/master/0x18-dynamic_libraries/100-operations.so
 export LD_PRELOAD="$PWD/../100-operations.so"
